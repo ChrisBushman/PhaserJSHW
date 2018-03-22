11f60dfbdf9367f5e776b7a2ffa51e8228db73a4
summerjw <summerjw@outlook.com>
2018-03-22 19:57:54 -0400

    Added animation code and assets

--- game.js
+++ game.js
@@ -2,11 +2,16 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
   game.load.image('square', 'assets/Images/square.png');
+  game.load.spritesheet('ship', 'assets/Images/sht_ship2.png', 48, 48);
   cursors = game.input.keyboard.createCursorKeys();
 }
 
 function create() {
-  player = game.add.image(370, 270, 'square');
+  player = game.add.image(370, 270, 'ship');
+  player.animations.add('moving', [1,2], 15, true);
+  player.animations.add('idle', [0], 0, false);
+  player.anchor.setTo(0.5, 0.5);
+  player.tint = Math.random() * 0xffffff;
 }
 
 function update() {
@@ -14,21 +19,44 @@ function update() {
   {
       //  Move to the left
       player.position.x = player.position.x - 5;
+      
+      //  Set animation to 'moving; rotate sprite to the left
+      player.animations.play('moving');
+      player.angle = 180;
+    
+    
   }
   else if (cursors.right.isDown)
   {
       //  Move to the right
        player.position.x = player.position.x + 5;
+    
+      //  Set animation to 'moving; rotate sprite to the right
+      player.animations.play('moving');
+      player.angle = 0;
   }
   
   else if (cursors.up.isDown)
   {
       //  Move to the left
       player.position.y = player.position.y - 5;
+    
+      //  Set animation to 'moving; rotate sprite to the right
+      player.animations.play('moving');
+      player.angle = 270;
   }
   else if (cursors.down.isDown)
   {
       //  Move to the right
        player.position.y = player.position.y + 5;
+    
+      //  Set animation to 'moving; rotate sprite to the right
+      player.animations.play('moving');
+      player.angle = 90;
+  }
+  else
+  {
+      // Player is idle; Play the idle animation
+      player.animations.play('idle');
   }
 }

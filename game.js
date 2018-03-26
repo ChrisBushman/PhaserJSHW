7b8cb3f37fff2599c13f6c0dfdf9a0ae102c8486
summerjw <summerjw@outlook.com>
2018-03-26 14:05:08 -0400

    updating the ship sprite

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-  game.load.image('square', 'assets/Images/square.png');
   game.load.spritesheet('ship', 'assets/Images/sht_ship2.png', 48, 48);
   cursors = game.input.keyboard.createCursorKeys();
 }
 
 function create() {
   player = game.add.image(370, 270, 'ship');
-  player.animations.add('moving', [1,2], 15, true);
+  
+  player.animations.add('moving', [1,2], 10, true);
   player.animations.add('idle', [0], 0, false);
   player.anchor.setTo(0.5, 0.5);
   player.tint = Math.random() * 0xffffff;
@@ -18,7 +18,7 @@ function update() {
   if (cursors.left.isDown)
   {
       //  Move to the left
-      player.position.x = player.position.x - 5;
+      player.position.x = player.position.x - 1;
       
       //  Set animation to 'moving; rotate sprite to the left
       player.animations.play('moving');
@@ -29,7 +29,7 @@ function update() {
   else if (cursors.right.isDown)
   {
       //  Move to the right
-       player.position.x = player.position.x + 5;
+       player.position.x = player.position.x + 1;
     
       //  Set animation to 'moving; rotate sprite to the right
       player.animations.play('moving');
@@ -39,7 +39,7 @@ function update() {
   else if (cursors.up.isDown)
   {
       //  Move to the left
-      player.position.y = player.position.y - 5;
+      player.position.y = player.position.y - 1;
     
       //  Set animation to 'moving; rotate sprite to the right
       player.animations.play('moving');
@@ -48,7 +48,7 @@ function update() {
   else if (cursors.down.isDown)
   {
       //  Move to the right
-       player.position.y = player.position.y + 5;
+       player.position.y = player.position.y + 1;
     
       //  Set animation to 'moving; rotate sprite to the right
       player.animations.play('moving');
